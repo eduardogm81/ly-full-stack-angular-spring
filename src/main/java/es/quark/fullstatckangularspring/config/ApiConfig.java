2e4209916a71eae64084dda7629420b90e98971b
eduardogm81qg <eduardogmqg@gmail.com>
2018-05-18 11:34:40 +0200

    Añadidos los métodos de la API

--- src/main/java/es/quark/fullstatckangularspring/config/ApiConfig.java
+++ src/main/java/es/quark/fullstatckangularspring/config/ApiConfig.java
@@ -3,6 +3,7 @@ package es.quark.fullstatckangularspring.config;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.ObjectWriter;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -11,7 +12,10 @@ public class ApiConfig {
 
     @Bean
     public ObjectMapper objectMapper() {
-        return new ObjectMapper();
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.registerModule(new JavaTimeModule());
+        return objectMapper;
+        // return new ObjectMapper();
     }
 
     @Bean
--- src/main/java/es/quark/fullstatckangularspring/rest/ReservationResource.java
+++ src/main/java/es/quark/fullstatckangularspring/rest/ReservationResource.java
@@ -1,21 +1,18 @@
 package es.quark.fullstatckangularspring.rest;
 
+import es.quark.fullstatckangularspring.model.request.ReservationRequest;
 import es.quark.fullstatckangularspring.model.response.ReservationResponse;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
 
 @RestController
 @RequestMapping(ResourceConstants.ROOM_RESERVATION_V1)
 public class ReservationResource {
-
 
     @RequestMapping(path = "", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
     public ResponseEntity<ReservationResponse> getAvailableRooms(
@@ -24,5 +21,22 @@ public class ReservationResource {
         return new ResponseEntity<>(new ReservationResponse(), HttpStatus.OK);
     }
 
+    @RequestMapping(path = "", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_UTF8_VALUE,
+            consumes = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    public ResponseEntity<ReservationResponse> createReservation(@RequestBody ReservationRequest reservationRequest) {
 
+        return new ResponseEntity<>(new ReservationResponse(), HttpStatus.CREATED);
+    }
+
+    @RequestMapping(path = "", method = RequestMethod.PUT, produces = MediaType.APPLICATION_JSON_UTF8_VALUE,
+            consumes = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    public ResponseEntity<ReservationResponse> updateReservation(@RequestBody ReservationRequest reservationRequest) {
+
+        return new ResponseEntity<>(new ReservationResponse(), HttpStatus.OK);
+    }
+
+    @RequestMapping(path = "/{reservationId}", method = RequestMethod.DELETE)
+    public ResponseEntity<Void> deleteReservation(@PathVariable long reservationId) {
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
 }
